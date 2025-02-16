6af0069e0bf8a8a71b0a77b95ed9c16d4837de1a
hmsou <hmarcs66@gmail.com>
2025-02-16 17:26:18 -0300

    Add Load Users

---
+++ src/main/java/com/hmso/blog/security/BlogUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.hmso.blog.security;
+
+import com.hmso.blog.domain.entities.User;
+import com.hmso.blog.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new BlogUserDetails(user);
+    }
+}
