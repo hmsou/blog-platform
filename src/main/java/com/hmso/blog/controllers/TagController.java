3e2606f231675ae55147f0e7c33e7450345ff1c7
hmsou <hmarcs66@gmail.com>
2025-02-17 22:58:41 -0300

    Add Delete Tag Endpoint

--- src/main/java/com/hmso/blog/controllers/TagController.java
+++ src/main/java/com/hmso/blog/controllers/TagController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/v1/tags")
@@ -41,6 +42,12 @@ public class TagController {
                 createdTagDtos,
                 HttpStatus.CREATED
         );
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable UUID id){
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/hmso/blog/services/TagService.java
+++ src/main/java/com/hmso/blog/services/TagService.java
@@ -4,8 +4,10 @@ import com.hmso.blog.domain.entities.Tag;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public interface TagService {
     List<Tag> listTags();
     List<Tag> createTag(Set<String> tagNames);
+    void deleteTag(UUID id);
 }
